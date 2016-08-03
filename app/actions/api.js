7217e22daf9a178a62e5a7e62b9391dd6399bcbb
vai <ncysatnaf@gmail.com>
2016-08-03 16:53:49 +0800

    build simple api flow

---
+++ app/actions/api.js
@@ -0,0 +1,62 @@
+import { CALL_API, Schemas } from '../middleware/api'
+
+const API = {
+  base: 'http://api.bilibili.com',
+  bangumi: 'http://app.bilibili.com/bangumi/timeline_v2'
+}
+
+function getHeader(){
+  return (
+       'Accept': 'application/json',
+       'Content-Type': 'application/json',
+     }
+   )
+}
+
+
+export const BANGUMI_REQUEST = 'BANGUMI_REQUEST'
+export const BANGUMI_SUCCESS = 'BANGUMI_SUCCESS'
+export const BANGUMI_FAILURE = 'BANGUMI_FAILURE'
+
+function fetchBangumi() {
+  return {
+    [CALL_API]: {
+      types: [ BANGUMI_REQUEST, BANGUMI_SUCCESS, BANGUMI_FAILURE ],
+      endpoint: API['bangumi'],
+      schema: Schemas.BANGUMI,
+      request: {
+        method: 'GET',
+        headers: getHeader()
+      }
+    }
+  }
+}
+
+export const loadBangumi = () => {
+  return (dispatch, getState) => {
+
+    return dispatch(fetchBangumi())
+  }
+}
+
+export const USER_REQUEST = 'USER_REQUEST'
+export const USER_SUCCESS = 'USER_SUCCESS'
+export const USER_FAILURE = 'USER_FAILURE'
+
+function fetchUser(name) {
+  return {
+    [CALL_API]: {
+      types: [ USER_REQUEST, USER_SUCCESS, USER_FAILURE ],
+      endpoint: `userinfo?user=${name}`,
+      schema: Schemas.USER
+    }
+  }
+}
+
+
+export function loadUser(name) {
+  return (dispatch, getState) => {
+
+    return dispatch(fetchUser(name))
+  }
+}
